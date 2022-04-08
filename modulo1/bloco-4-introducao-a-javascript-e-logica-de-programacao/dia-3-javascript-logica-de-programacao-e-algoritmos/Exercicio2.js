80e6d1c3a2ba1d84f46c97b48edf0178fb2cf094
biancabera <biancabera@gmail.com>
2022-04-08 18:47:22 -0300

    exercicio 1 e 2

---
+++ modulo1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exercicio2.js
@@ -0,0 +1,8 @@
+let word = 'tryber';
+let palavraInvertida = "";
+
+for (let index = word.length - 1; index >= 0; index -= 1) {
+    palavraInvertida = palavraInvertida + word[index]
+    }
+
+    console.log(palavraInvertida)
