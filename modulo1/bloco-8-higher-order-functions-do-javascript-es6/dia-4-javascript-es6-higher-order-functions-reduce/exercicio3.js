fc6569566df16b41ee439a9d1281ab6e9705ab3d
biancabera <biancabera@gmail.com>
2022-05-18 22:18:15 -0300

    Correção

--- modulo1/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio3.js
+++ modulo1/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio3.js
@@ -70,18 +70,3 @@ function averageAge() {
   return idadeLancamento / books.length;
 } 
 console.log(averageAge(books));
-
-
-  // 🚀 5- Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
-  // const names = [
-  //   'Aanemarie', 'Adervandes', 'Akifusa',
-  //   'Abegildo', 'Adicellia', 'Aladonata',
-  //   'Abeladerco', 'Adieidy', 'Alarucha',
-  // ];
-  
-  // function containsA() {
-  //   names.reduce(acc, curr) => {
-  //     if (curr === 'a' || curr === 'A') return acc + 1;
-  //     return acc;
-  //   }
-  // }
